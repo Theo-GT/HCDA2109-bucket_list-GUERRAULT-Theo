3a05ccc4a07814e0c7f4669a0ae5d07eae08e10a
name <103489208+dracouche@users.noreply.github.com>
2022-09-08 12:08:03 +0200

    up

---
+++ src/Controller/MainController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/legal-stuff", name="app_")
+ */
+class MainController extends AbstractController
+{
+
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @Route("/aboutus", name="aboutus")
+     */
+    public function aboutUs(): Response
+    {
+        return $this->render('main/about-us.html.twig');
+    }
+
+
+
+    /**
+     * @Route("/", name="main")
+     */
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+}
